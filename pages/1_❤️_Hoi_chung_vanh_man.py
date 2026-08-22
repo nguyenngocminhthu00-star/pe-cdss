4d0c4b9fc244a4928d0cc5350f2f1e1f6f205fb2
nguyenngocminhthu00-star <nguyenngocminhthu00@gmail.com>
2026-08-22 10:21:43 +0700

    Update 1_❤️_Hoi_chung_vanh_man.py

--- pages/1_❤️_Hoi_chung_vanh_man.py
+++ pages/1_❤️_Hoi_chung_vanh_man.py
@@ -7,6 +7,16 @@ st.set_page_config(
     page_icon="🫀",
     layout="wide"
 )
+# MENU ĐIỀU HƯỚNG
+st.sidebar.page_link(
+    "pe-cdss-web.py",
+    label="🫁 Thuyên tắc phổi cấp"
+)
+
+st.sidebar.page_link(
+    "pages/1_❤️_Hoi_chung_vanh_man.py",
+    label="❤️ Hội chứng vành mạn"
+)
 
 # Custom Styling for cardiology clinical aesthetics
 st.markdown("""
