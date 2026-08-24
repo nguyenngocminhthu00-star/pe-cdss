a3dbfc2bbe70ddef7bcc7baab131b7766e527a13
nguyenngocminhthu00-star <nguyenngocminhthu00@gmail.com>
2026-08-24 16:05:03 +0700

    Update pe-cdss-web.py

--- pe-cdss-web.py
+++ pe-cdss-web.py
@@ -6,36 +6,45 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-# =========================
-# KHAI BÁO CÁC TRANG
-# =========================
+# =========================================================
+# KHAI BÁO CÁC TOOL
+# =========================================================
 
 pe = st.Page(
-    "pages/0_🫁_Thuyen_tac_phoi.py",
+    "pages/0_🫁_Thuyên_tắc_phổi.py",
     title="Thuyên tắc phổi cấp",
     icon="🫁",
     default=True
 )
 
 ccs = st.Page(
-    "pages/1_❤️_Hoi_chung_vanh_man.py",
+    "pages/1_❤️_Hội_chứng_vành_mạn.py",
     title="Hội chứng vành mạn",
     icon="❤️"
 )
 
 lipid = st.Page(
-    "pages/2_🩸_Roi_loan_lipid_mau.py",
+    "pages/2_🩸_Rối_loạn_lipid_máu.py",
     title="Rối loạn lipid máu",
     icon="🩸"
 )
 
-# =========================
+# =========================================================
 # MENU TRUNG TÂM
-# =========================
+# =========================================================
 
 menu = st.navigation(
-    [pe, ccs, lipid],
-    position="sidebar"
+    [
+        pe,
+        ccs,
+        lipid
+    ],
+    position="sidebar",
+    expanded=True
 )
+
+# =========================================================
+# CHẠY TOOL ĐƯỢC CHỌN
+# =========================================================
 
 menu.run()
