9267cc6b6f9029df2caed3865fd90208723f6563
nguyenngocminhthu00-star <nguyenngocminhthu00@gmail.com>
2026-08-24 14:53:44 +0700

    Create 2_🩸_Roi_loan_lipid_mau.py

---
+++ pages/2_🩸_Roi_loan_lipid_mau.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Rối loạn lipid máu",
+    page_icon="🩸",
+    layout="wide"
+)
+
+st.title("🩸 TIẾP CẬN RỐI LOẠN LIPID MÁU")
+
+st.caption("Hệ thống Hỗ trợ Quyết định Lâm sàng theo ACC/AHA 2026")
+
+st.info("Công cụ đang được xây dựng.")
